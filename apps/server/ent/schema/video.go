076b4cdc57d016c7b1c2e0770ad73b7e0240cd38
expesh <104657457+expesh@users.noreply.github.com>
2024-04-11 15:03:31 +0300

    feat(server): initialize server code

---
+++ apps/server/ent/schema/video.go
@@ -0,0 +1,36 @@
+package schema
+
+import (
+	"time"
+
+	"entgo.io/contrib/entgql"
+	"entgo.io/ent"
+	"entgo.io/ent/schema"
+	"entgo.io/ent/schema/field"
+)
+
+// Video holds the schema definition for the Video entity.
+type Video struct {
+	ent.Schema
+}
+
+// Fields of the Video.
+func (Video) Fields() []ent.Field {
+	return []ent.Field{
+		field.String("path"),
+		field.Time("created_at").
+			Default(time.Now),
+	}
+}
+
+// Edges of the Video.
+func (Video) Edges() []ent.Edge {
+	return nil
+}
+
+func (Video) Annotations() []schema.Annotation {
+	return []schema.Annotation{
+		entgql.QueryField(),
+		entgql.Mutations(entgql.MutationCreate()),
+	}
+}
